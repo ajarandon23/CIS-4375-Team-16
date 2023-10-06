b90b19c7322c34f6f84ddd99aa6171257d1ee2c6
izthemann92 <82607762+izthemann92@users.noreply.github.com>
2023-10-06 15:40:05 -0500

    I am trying

--- pm-app/src/main.js
+++ pm-app/src/main.js
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 const app = createApp(App)
 
 app.use(createPinia())
