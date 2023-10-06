4fdac82b260ed7e8c8b1ac50080f3fcea4de94da
izthemann92 <82607762+izthemann92@users.noreply.github.com>
2023-10-06 14:03:50 -0500

    started working on frontend

---
+++ Productivity_management_app/Production_management_app/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="flex flex-row">
+    <div id="_container" class="h-screen">
+      <header class="w-full">
+        <section class="text-center">
+          img
+        </section>
+
+      </header>
+
+    </div>
+
+  </main>
+
+</template>
+
+<style scoped>
+#_container {
+  background-color: #c8102e;
+  color: white;
+  padding: 18px;
+}
+</style>
